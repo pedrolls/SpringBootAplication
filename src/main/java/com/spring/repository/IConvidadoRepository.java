8eac37e2764bd6c04fdc3a7b7b60de2f4eceb1fe
pedrolls <silva418@hotmail.com>
2021-04-11 18:06:07 -0300

    Exibindo lista de convidados de forma simples.

--- src/main/java/com/spring/repository/IConvidadoRepository.java
+++ src/main/java/com/spring/repository/IConvidadoRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.spring.model.Convidado;
+import com.spring.model.Evento;
 
 @Repository
 public interface IConvidadoRepository extends CrudRepository<Convidado, String>{
 
+	Iterable<Convidado> findByEvento(Evento evento);
 }
